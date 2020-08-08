a131c85b2d531b0f050c27cdfe8292ccb6ab8542
MuhammadIchsan21 <ichsanteratai@gmail.com>
2020-08-08 14:42:19 +0800

    Perbarui lagi guys

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,11 +1,11 @@
+import "../component/club-list.js";
+import "../component/search-bar.js";
 import DataSource from "../data/data-source.js";
 
 const main = () => {
-  const searchElement = document.querySelector("#searchElement");
-  const buttonSearchElement = document.querySelector("#searchButtonElement");
-  const clubListElement = document.querySelector("#clubList");
+  const searchElement = document.querySelector("search-bar");
+  const clubListElement = document.querySelector("club-list");
 
-  //Menggunakan Asyncs/Await
   const onButtonSearchClicked = async () => {
     try {
       const result = await DataSource.searchClub(searchElement.value);
@@ -14,36 +14,16 @@ const main = () => {
       fallbackResult(message);
     }
   };
+
   const renderResult = (results) => {
-    clubListElement.innerHTML = "";
-    results.forEach((club) => {
-      const name = club.name;
-      const fanArt = club.fanArt;
-      const description = club.description;
-
-      const clubElement = document.createElement("div");
-      clubElement.setAttribute("class", "club");
-
-      clubElement.innerHTML = `<img class="fan-art-club" src=
-        ${fanArt} 
-        alt="Fan Art"> 
-        <div class="club-info"> 
-        <h2>
-        ${name}
-        </h2>
-        <p> 
-        ${description} 
-        </p>
-        </div>`;
-      clubListElement.appendChild(clubElement);
-    });
+    clubListElement.clubs = results;
   };
 
   const fallbackResult = (message) => {
-    clubListElement.innerHTML = "";
-    clubListElement.innerHTML += `<h2 class="placeholder">${message} </h2>`;
+    clubListElement.renderError(message);
   };
 
-  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  searchElement.clickEvent = onButtonSearchClicked;
 };
+
 export default main;
